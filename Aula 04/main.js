9ba049985b4c43b4e4dab6590e63fb5444fe58e0
Sara Anita <sara.anita.desa@gmail.com>
2023-06-25 13:54:18 -0300

    Aprendendo operadores aritiméticos em js

---
+++ Aula 04/main.js
@@ -0,0 +1,5 @@
+window.alert("Minha Primeira Mensagem");
+
+window.confirm("Está gostando de JavaScript"); //Janela com Botão OK e Cancelar
+
+window.prompt("Qual é o seu nome?"); // vai perguntar o nome
---
+++ Aula 06/main2.js
@@ -0,0 +1,10 @@
+// window.alert("Minha Primeira Mensagem");
+
+// window.confirm("Está gostando de JavaScript"); //Janela com Botão OK e Cancelar
+
+// window.prompt("Qual é o seu nome?"); // vai perguntar o nome
+
+var nome = window.prompt("Qual é o seu nome?");
+window.alert("É um grande prazer em te conhecer," + nome + "!");
+
+
