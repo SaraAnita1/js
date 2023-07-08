4e1cbffaf2e96854f9d6f50d6015d025b91848fc
Sara Anita <sara.anita.desa@gmail.com>
2023-07-08 00:51:22 -0300

    Aprendendo condições Aninhadas

---
+++ Aula 12/ex0012.js
@@ -0,0 +1,13 @@
+var hora = 10
+
+console.log(`Agora São exatamente ${hora} horas.`)
+
+if (hora <= 5) {
+    console.log('Tu não é morcega e.e. Vai dormir e.e 😡')
+} else if (hora < 12) {
+    console.log('Bom Dia! O sol já queimou a fazendinha 😊')
+} else if (hora <= 18) {
+    console.log('Boa Tarde! Hora do Cafê ☕')
+} else {
+    console.log('Boa Noite! ')
+}
