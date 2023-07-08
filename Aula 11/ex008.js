237c003fe6d5bc2c245de040d9e9fe5c06b023b0
Sara Anita <sara.anita.desa@gmail.com>
2023-07-07 22:41:44 -0300

    Explorando funções do javascript

---
+++ Aula 11/ex008.js
@@ -0,0 +1,8 @@
+// console.log('o console funcionou corretamente!')
+
+var vel = 62
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if(vel > 60) { //condição simples!
+    console.log(`Você ultrapassou a velocidade permitida, MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
