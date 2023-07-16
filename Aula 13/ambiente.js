182670b3bf145d3d52e00eb31ef869bc685afa06
Sara Anita <sara.anita.desa@gmail.com>
2023-07-16 14:41:43 -0300

    modificando locais em que exercicios estão

---
+++ Aula 13/ambiente.js
@@ -0,0 +1,16 @@
+/* estrutura de repetição com teste logico no inicio
+
+var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++
+}*/
+
+/*console.log('tudo bem?')*/
+
+/* estrutura de repetição com teste logico no final */
+var c = 1 
+do {
+        console.log(`Passo ${c}`)
+        c++
+} while (c <= 6)
